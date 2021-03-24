49b811f822098ec4ac9a528cde128981460a22e3
Luciano Domingues <52328978+luccidomingues@users.noreply.github.com>
2021-03-23 20:28:00 -0600

    Add files via upload

--- ibov-app.py
+++ ibov-app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import base64
 # import matplotlib.pyplot as plt
 import matplotlib as plt
-import seaborn as sns
-import numpy as np
+# import seaborn as sns
+# import numpy as np
 import yfinance as yf
 
 st.title('IBOVESPA App')
