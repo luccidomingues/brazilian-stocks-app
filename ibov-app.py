3d9923cc266d1282bf22f424977e35b08165486c
Luciano Domingues <52328978+luccidomingues@users.noreply.github.com>
2021-04-14 06:42:36 -0500

    Update ibov-app.py

--- ibov-app.py
+++ ibov-app.py
@@ -111,7 +111,7 @@ def price_plot(symbol):
   plt.ylabel('Closing Price', fontweight='bold')
   return st.pyplot()
 
-num_company = st.sidebar.slider('Number of Companies', 1, 10)
+num_company = st.sidebar.slider('Number of Companies', 1, 10, 1, 1)
 
 if st.button('Show Plots'):
     st.header('Stock Closing Price')
