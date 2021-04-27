68f82e092025def06df29a8540048b19bfad3edc
Luciano Domingues <52328978+luccidomingues@users.noreply.github.com>
2021-04-27 09:33:58 -0500

    Add files via upload

--- ibov-app.py
+++ ibov-app.py
@@ -88,9 +88,17 @@ st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
 num_company = st.sidebar.slider('Number of Companies', 1, 10, 1, 1)
 
 
+
+
+
+
+st.write('Lista ' + list(df_selected_sector[:10].Ticker+'.SA'))
+
+
+
+
+
 # https://pypi.org/project/yfinance/
-
-
 
 # data = yf.download(
 #         tickers = list(df_selected_sector[:10].Ticker+'.SA'),
@@ -113,7 +121,7 @@ data = yf.download(
         tickers = list(df_selected_sector.Ticker+'.SA'),
         period = "2y",
         interval = "1d",
-        group_by = 'ticker',
+        group_by = 'Ticker',
         auto_adjust = True,
         prepost = True,
         threads = True,
@@ -121,6 +129,42 @@ data = yf.download(
     )
 
 
+
+
+# data = yf.download(  # or pdr.get_data_yahoo(...
+#         # tickers list or string as well
+#         tickers = "SPY AAPL MSFT",
+
+#         # use "period" instead of start/end
+#         # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+#         # (optional, default is '1mo')
+#         period = "ytd",
+
+#         # fetch data by interval (including intraday if period < 60 days)
+#         # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+#         # (optional, default is '1d')
+#         interval = "1m",
+
+#         # group by ticker (to access via data['SPY'])
+#         # (optional, default is 'column')
+#         group_by = 'ticker',
+
+#         # adjust all OHLC automatically
+#         # (optional, default is False)
+#         auto_adjust = True,
+
+#         # download pre/post regular market hours data
+#         # (optional, default is False)
+#         prepost = True,
+
+#         # use threads for mass downloading? (True/False/Integer)
+#         # (optional, default is True)
+#         threads = True,
+
+#         # proxy URL scheme use use when downloading?
+#         # (optional, default is None)
+#         proxy = None
+#     )
 
 
 # Luciano
